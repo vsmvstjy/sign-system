60e56ceff03c4630e64a63b76689b508dfe2222a
vsmvstjy <50073995+vsmvstjy@users.noreply.github.com>
2025-04-25 22:41:55 +0900

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,14 +1,18 @@
-export const firebaseConfig = {
+// Firebase configuration
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.22.1/firebase-app.js';
+import { getFirestore } from 'https://www.gstatic.com/firebasejs/9.22.1/firebase-firestore.js';
+
+const firebaseConfig = {
   apiKey: "AIzaSyDrkWBu5k-2nBHhZEHwgrBrPkWSKpro6po",
   authDomain: "sign-system-a8921.firebaseapp.com",
   projectId: "sign-system-a8921",
   storageBucket: "sign-system-a8921.appspot.com",
   messagingSenderId: "356422555230",
-  appId: "1:356422555230:web:5522aefcfc133d096286cf"
+  appId: "1:356422555230:web:5522aefcfc133d096286cf",
+  measurementId: "G-KD90T9QP1W"
 };
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.1/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/9.22.1/firebase-firestore.js";
-import { getStorage } from "https://www.gstatic.com/firebasejs/9.22.1/firebase-storage.js";
+
 const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
-export const storage = getStorage(app);
+const db = getFirestore(app);
+
+export { db };
