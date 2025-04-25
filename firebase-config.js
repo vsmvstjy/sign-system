ab49584dc78d6999c936c3e270649d06096baf43
vsmvstjy <50073995+vsmvstjy@users.noreply.github.com>
2025-04-25 22:32:40 +0900

    Add files via upload

--- firebase-config.js
+++ firebase-config.js
@@ -1,18 +1,14 @@
-// Firebase configuration
-import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.22.1/firebase-app.js';
-import { getFirestore } from 'https://www.gstatic.com/firebasejs/9.22.1/firebase-firestore.js';
-
-const firebaseConfig = {
+export const firebaseConfig = {
   apiKey: "AIzaSyDrkWBu5k-2nBHhZEHwgrBrPkWSKpro6po",
   authDomain: "sign-system-a8921.firebaseapp.com",
   projectId: "sign-system-a8921",
   storageBucket: "sign-system-a8921.appspot.com",
   messagingSenderId: "356422555230",
-  appId: "1:356422555230:web:5522aefcfc133d096286cf",
-  measurementId: "G-KD90T9QP1W"
+  appId: "1:356422555230:web:5522aefcfc133d096286cf"
 };
-
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.1/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/9.22.1/firebase-firestore.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/9.22.1/firebase-storage.js";
 const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
-
-export { db };
+export const db = getFirestore(app);
+export const storage = getStorage(app);
