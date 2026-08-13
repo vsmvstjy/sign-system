4c0204cd6dc14ba84c563d274b2deca486bb44c3
vsmvstjy <50073995+vsmvstjy@users.noreply.github.com>
2026-08-14 08:02:59 +0900

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -3,13 +3,12 @@ import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.22.1/firebas
 import { getFirestore } from 'https://www.gstatic.com/firebasejs/9.22.1/firebase-firestore.js';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDrkWBu5k-2nBHhZEHwgrBrPkWSKpro6po",
-  authDomain: "sign-system-a8921.firebaseapp.com",
-  projectId: "sign-system-a8921",
-  storageBucket: "sign-system-a8921.appspot.com",
-  messagingSenderId: "356422555230",
-  appId: "1:356422555230:web:5522aefcfc133d096286cf",
-  measurementId: "G-KD90T9QP1W"
+  apiKey: "AIzaSyBJacrTbuM7Vfi83xKVA04-zoGXnwi_McU",
+  authDomain: "sign-457723.firebaseapp.com",
+  projectId: "sign-457723",
+  storageBucket: "sign-457723.firebasestorage.app",
+  messagingSenderId: "972595541590",
+  appId: "1:972595541590:web:3701e82ad02a1db3b52df7"
 };
 
 const app = initializeApp(firebaseConfig);
